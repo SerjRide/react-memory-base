736049089c7428666e7ab0c09480e396343e372e
SerjRide <quicksquicks6@gmail.com>
2019-03-04 23:41:14 +0300

    little change

--- src/components/categories/categories.js
+++ src/components/categories/categories.js
@@ -36,7 +36,6 @@ export default class Categories extends Component {
   };
 
   renameCategory = (id) => {
-    console.log('rename');
     this.props.getCategoryRename()
     const { value } = document.getElementById(`rename_${id}`)
     rename(id, value);
